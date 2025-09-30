d697de20fa971582eecbdda13bb165df644569b6
Sahil Kamble <syk2448@gmail.com>
2025-09-30 15:58:15 +0530

    pushing for deployment

--- Backend/src/db/knex.ts
+++ Backend/src/db/knex.ts
@@ -1,6 +1,6 @@
 // src/db/knex.ts
 import knex from "knex";
-import config from "../../knexfile"; // <-- removed .ts extension
+import config from "./knexfile"; // <-- removed .ts extension
 import type { Knex } from "knex";
 
 const environment = (process.env.NODE_ENV as string) || "development";
