ece24553469d988a838ec088260c80a696e787ef
Sahil Kamble <syk2448@gmail.com>
2025-09-30 16:15:29 +0530

    testing

--- Backend/src/db/knex.ts
+++ Backend/src/db/knex.ts
@@ -1,11 +1,14 @@
 // src/db/knex.ts
 import knex from "knex";
-import config from "./knexfile"; // <-- removed .ts extension
+import config from "../knexfile"; // <-- removed .ts extension
 import type { Knex } from "knex";
 
 const environment = (process.env.NODE_ENV as string) || "development";
 const cfg = config[environment] as Knex.Config;
+console.log('Knex config:', config);
+console.log('Environment:', process.env.NODE_ENV);
 
 const db = knex(cfg);
+
 
 export default db;
